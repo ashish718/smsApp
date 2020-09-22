79c0b5af6f83958043c81d471239c40da229f651
Ashish Goyal <goyal.ashish062@gmail.com>
2020-09-22 18:52:03 +0530

    structure testing 1

--- index.js
+++ index.js
@@ -94,12 +94,12 @@ cron.schedule('*/2 * * * * ', async () => {
 
 
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
-}
+//}
 
 
 // app.post('/whatsapp', function(req, res) {

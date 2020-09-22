0cb3e1b3b3447a86cdd36de30f3bb8bf81cc8c4f
Ashish Goyal <goyal.ashish062@gmail.com>
2020-09-22 19:46:27 +0530

    structure testing 3

---
+++ client/build/precache-manifest.b5c5120569c5a6e6678650f971274b75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70b2009bab6f382f6a044bdd6af616c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78a2fe0fda2c42cfb8de",
+    "url": "/static/css/2.f1bd7250.chunk.css"
+  },
+  {
+    "revision": "2d53ea99c783f71f9959",
+    "url": "/static/css/main.81efa1c6.chunk.css"
+  },
+  {
+    "revision": "78a2fe0fda2c42cfb8de",
+    "url": "/static/js/2.0bb516e3.chunk.js"
+  },
+  {
+    "revision": "2d53ea99c783f71f9959",
+    "url": "/static/js/main.fafc0165.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,6 @@ import {AppProvider} from "@shopify/polaris";
 import en from '@shopify/polaris/locales/en.json';
 
 import App from "./App";
-import "@shopify/polaris/styles.css";
+import "@shopify/polaris/dist/styles.css";
 
 ReactDOM.render(<AppProvider i18n={en}><App /></AppProvider>, document.getElementById("root"));
